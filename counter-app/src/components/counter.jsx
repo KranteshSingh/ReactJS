9037f8db817d8c3533555812b41ee2f99bea459f
Krantesh Singh <34528823+KranteshSingh@users.noreply.github.com>
2019-10-14 08:31:20 +0530

    Counter App

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -3,9 +3,14 @@
 import React, { Component } from 'react';
 class Counter extends Component {
   // state is a speacial property aka object in React, it includes any data that this component needs
+  // props includes the data that we give to a component
+  // state contains the data that is local or private to a component
+  // props is read only
+  /*
   state = {
-    count: 0
+    value: this.props.counter.value
   };
+  */
 
   style = {
     fontSize: '15px',
@@ -15,28 +20,47 @@ class Counter extends Component {
 
   // Format of the Counter if its 0 then it will display it in Strings
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.props.counter;
     return count === 0 ? 'ZERO' : count;
   }
 
   // Handling Events
   // Updating the State of Badge
+  /*
   handleIncrement = () => {
     console.log('+1', this);
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   handleDecrement = () => {
-    this.setState({ count: this.state.count - 1 });
+    this.setState({ value: this.state.value - 1 });
   };
+  */
 
   // Rendering Classes Dynamically
   getBadgeClasses() {
     let classes = 'badge m-2 ';
-    classes += this.state.count === 0 ? 'badge-warning' : 'badge-primary';
+    classes +=
+      this.props.counter.value === 0 ? 'badge-warning' : 'badge-primary';
     return classes;
   }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log('prevProps', prevProps);
+    console.log('prevState', prevState);
+    if (prevProps.counter.value !== this.props.counter.value) {
+      // Ajax call and get new data from the server
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('Counter - Unmount');
+  }
   render() {
+    // Every react component has property called props. It is just a plain JS Objects hold all the attributes we defined in the Counter components
+    // Here Counter component is raising the event and its parent component is handling the event.
+    console.log('Counter Rendered');
+    //console.log('props', this.props);
     return (
       <div style={this.style} className="container">
         <h1
@@ -45,21 +69,39 @@ class Counter extends Component {
           Kounter Singh
         </h1>
         <hr />
-        <span style={{ fontSize: '17px' }} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button
-          onClick={this.handleIncrement}
-          className="btn btn-secondary btn-md"
-        >
-          INCREMENT
-        </button>
-        <button
-          onClick={this.handleDecrement}
-          className="btn btn-danger btn-md m-2"
-        >
-          DECREMENT
-        </button>
+
+        <div className="row">
+          <div className="col-1">
+            <span
+              style={{ fontSize: '17px' }}
+              className={this.getBadgeClasses()}
+            >
+              {this.formatCount()}
+            </span>
+          </div>
+          <div className="col">
+            <button
+              onClick={() => this.props.onIncrement(this.props.counter)}
+              className="btn btn-secondary btn-md"
+            >
+              +
+            </button>
+
+            <button
+              onClick={() => this.props.onDecrement(this.props.counter)}
+              className="btn btn-danger btn-md m-2"
+              disabled={this.props.counter.value === 0 ? 'disabled' : ''}
+            >
+              -
+            </button>
+            <button
+              onClick={() => this.props.onDelete(this.props.counter.id)}
+              className="btn btn-danger btn-md"
+            >
+              DELETE
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ counter-app/src/components/counters.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+class Counters extends Component {
+  render() {
+    // Object Destructuring
+    const {
+      onReset,
+      counters,
+      onDelete,
+      onIncrement,
+      onDecrement
+    } = this.props;
+
+    console.log('Counters Rendered');
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">
+          RESET
+        </button>
+        {counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            counter={counter}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
