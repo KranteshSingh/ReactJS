28b02967a451ae0f5046200c9bbd6ea6709466b2
Krantesh Singh <34528823+KranteshSingh@users.noreply.github.com>
2019-11-02 08:55:50 +0530

    React App

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { getMovies } from '../services/fakeMovieService';
-import _ from 'lodash';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import MoviesTable from './moviesTable';
 import ListGroup from './common/listGroup';
 import Pagination from './common/pagination';
-import { getGenres } from '../services/fakeGenreService';
+import { getMovies, deleteMovie } from '../services/movieService';
+import { getGenres } from '../services/genreService';
 import { paginate } from '../utlis/paginate';
-import MoviesTable from './moviesTable';
-import { Link } from 'react-router-dom';
+import _ from 'lodash';
 import SearchBox from './searchBox';
 
 class Movies extends Component {
@@ -20,15 +21,27 @@ class Movies extends Component {
     sortColumn: { path: 'title', order: 'asc' }
   };
 
-  componentDidMount() {
-    const genres = [{ _id: '', name: 'All Genres' }, ...getGenres()];
-    this.setState({ movies: getMovies(), genres });
+  async componentDidMount() {
+    const { data } = await getGenres();
+    const genres = [{ _id: '', name: 'All Genres' }, ...data];
+
+    const { data: movies } = await getMovies();
+    this.setState({ movies, genres });
   }
 
-  handleDelete = movie => {
-    console.log(movie);
-    const movies = this.state.movies.filter(m => m._id !== movie._id);
+  handleDelete = async movie => {
+    const originalMovies = this.state.movies;
+    const movies = originalMovies.filter(m => m._id !== movie._id);
     this.setState({ movies });
+
+    try {
+      await deleteMovie(movie._id);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 404)
+        toast.error('This movie has already been deleted');
+
+      this.setState({ movies: originalMovies });
+    }
   };
 
   handleLiked = movie => {
